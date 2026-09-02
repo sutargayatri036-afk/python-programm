db37aaead917a23c7fa765aa27f905d3c03e26a3
sutargayatri036-afk <sutargayatri036@gmail.com>
2026-09-02 15:37:35 +0530

    Add files via upload

---
+++ joke.py
@@ -0,0 +1,10 @@
+import random
+
+jokes = [
+    "Why did the computer go to the doctor? It had a virus! 🤣",
+    "Why was the math book sad? It had too many problems! 😂",
+    "Why do programmers prefer dark mode? Because light attracts bugs! 🐛",
+    "What do you call a computer that sings? A-Dell! 🎵"
+]
+
+print(random.choice(jokes))
